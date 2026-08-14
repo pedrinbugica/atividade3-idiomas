d2080fb5cdf65852338d4955c1fd6e604a5cbb49
pedrinbugica <kantorrike@gmail.com>
2026-08-14 15:42:48 -0300

    Subindo projeto

---
+++ src/main/java/com/example/atividade3_idiomas/Atividade3IdiomasApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividade3_idiomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade3IdiomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade3IdiomasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atividade3_idiomas/Atividade3IdiomasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade3_idiomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade3IdiomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
